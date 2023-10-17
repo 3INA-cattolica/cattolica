fed58c9df6787ba67b30833e089b4f7580b416f3
3INA-cattolica <146919251+3INA-cattolica@users.noreply.github.com>
2023-10-17 20:48:05 +0200

    Update TriangoloScalenoTest.java

--- src/figureGeometriche/TriangoloScalenoTest.java
+++ src/figureGeometriche/TriangoloScalenoTest.java
@@ -1,20 +1,18 @@
-
 package figureGeometriche;
-
 
 public class TriangoloScalenoTest {
 
-    
     public static void main(String[] args) {
         TriangoloScaleno t1 = new TriangoloScaleno(3,4,5);
+        
+        System.out.println("info              : "+ t1.info());
         double t = t1.perimetro();
         System.out.println("il perimetro è di : "+ t);
-        
         double a = t1.area();
         System.out.println("l'area è di       : " + a);
-        
         double h = t1.trovaAltezza();
         System.out.println("l'altezza è di    : " + h);
     }
     
 }
+
