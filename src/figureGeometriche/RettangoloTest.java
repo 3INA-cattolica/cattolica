f9930c08f326c14a8a98330ec3d43810794cd95f
3INA-cattolica <146919251+3INA-cattolica@users.noreply.github.com>
2023-10-17 20:45:19 +0200

    Update RettangoloTest.java

--- src/figureGeometriche/RettangoloTest.java
+++ src/figureGeometriche/RettangoloTest.java
@@ -1,11 +1,9 @@
-
 package figureGeometriche;
 
 import java.util.Scanner;
 
 public class RettangoloTest {
 
-    
     public static void main(String[] args) {
         Scanner tastiera = new Scanner(System.in);
         double base;
@@ -20,11 +18,23 @@ public class RettangoloTest {
         
         double area = r1.area();
         double perimetro = r1.perimetro();
+        String info = r1.info();
         
+        System.out.println("info                          : " +info);
         System.out.println("l'area del cerchio è di       : " +area);
         System.out.println("il perimetro del cerchio è di : " +perimetro);
-
         
+        System.out.println("inseire la nuova base : ");
+        base = tastiera.nextDouble();
+        r1.setBase(base);
+        System.out.println("inseire la nuova altezza : ");
+        altezza = tastiera.nextDouble();
+        r1.setAltezza(altezza);
+        
+        System.out.println("info                          : " +info);
+        System.out.println("l'area del cerchio è di       : " +area);
+        System.out.println("il perimetro del cerchio è di : " +perimetro);
+       
     }
     
 }
