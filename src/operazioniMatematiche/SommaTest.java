5f2c34b3dd4b7506157ea3d76e19ee8f3733aca8
3INA-cattolica <146919251+3INA-cattolica@users.noreply.github.com>
2023-10-17 20:41:54 +0200

    Update SommaTest.java

--- src/operazioniMatematiche/SommaTest.java
+++ src/operazioniMatematiche/SommaTest.java
@@ -6,13 +6,19 @@ public class SommaTest {
         
         Somma somma = new Somma(8,9.3);
         double s = somma.esegui();
-        double n1  = somma.n1;
-        double n2 = somma.n2;
+        double n1  = somma.getN1();
+        double n2 = somma.getN2();
+        String info = somma.info();
         
         System.out.println(s);
         System.out.println(somma.esegui());
         System.out.println("numero1 : " + n1);
         System.out.println("numero2 : " + n2);
+        System.out.println("somma   : " + s);
+        
+        somma.setN1(10);
+        somma.setN2(17);
+        System.out.println(info);
         System.out.println("somma   : " + s);
         
         
