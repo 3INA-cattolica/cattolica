17785e7a3574300db364175faf559685c6646edf
3INA-cattolica <146919251+3INA-cattolica@users.noreply.github.com>
2023-10-17 20:43:28 +0200

    Update Cerchio.java

--- src/figureGeometriche/Cerchio.java
+++ src/figureGeometriche/Cerchio.java
@@ -1,16 +1,28 @@
 package figureGeometriche;
 
-
 public class Cerchio {
-    public double r;
-    public double p;
-    
-    
+    private double r;
+    private double p;
     
     public Cerchio(double raggio){
         r = raggio;
-        p = 3.14;
-        
+        p = Math.PI;
+    }
+    
+     public double getR(){
+        return this.r;
+    }
+    
+    public void setR(double r){
+        this.r = r;
+    }
+    
+    public double getp(){
+        return this.p;
+    }
+    
+    public void setP(double p){
+        this.p = p;
     }
     
     public double area(){
@@ -23,6 +35,12 @@ public class Cerchio {
         double ritorno;
         ritorno = 2* p* r;
         return ritorno;
+    }
+    
+    public String info(){
+        String stringa;
+        stringa =  "raggio: " + this.r ;
+        return stringa;
     }
     
     
