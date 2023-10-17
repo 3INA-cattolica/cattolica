ee4b135170efe172a9c39894d62ba2376795dfe9
3INA-cattolica <146919251+3INA-cattolica@users.noreply.github.com>
2023-10-17 20:40:39 +0200

    Update Somma.java

--- src/operazioniMatematiche/Somma.java
+++ src/operazioniMatematiche/Somma.java
@@ -1,13 +1,31 @@
 package operazioniMatematiche;
 
+import figureGeometriche.TrapezioIsoscele;
+
 
 public class Somma {
-    public double n1;
-    public double n2;
+    private double n1;
+    private double n2;
     
     public Somma(double numero1,double numero2){
-        n1 = numero1;
-        n2 = numero2;
+        this.n1 = numero1;
+        this.n2 = numero2;
+    }
+    
+    public double getN1(){
+        return this.n1;
+    }
+    
+    public void setN1(double n1){
+        this.n1 = n1;
+    }
+    
+    public double getN2(){
+        return this.n2;
+    }
+    
+    public void setN2(double n2){
+        this.n2 = n2;
     }
     
     public double esegui(){
@@ -15,4 +33,12 @@ public class Somma {
         somma = n1 + n2;
         return somma;
     }
+    
+    public String info(){
+        String stringa;
+        stringa = "\n" + "primo numero: " + this.n1 + "\n" + 
+              "secondo numero  : " + this.n2 ;
+        return stringa;
+    }
+    
 }
