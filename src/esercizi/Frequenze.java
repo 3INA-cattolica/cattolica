5b37547eefe0be7e86f192c9b881c8ade55d0243
3INA-cattolica <146919251+3INA-cattolica@users.noreply.github.com>
2024-01-24 10:17:29 +0100

    Add files via upload

---
+++ src/esercizi/Frequenze.java
@@ -0,0 +1,33 @@
+package esercizi;
+
+/**
+ *
+ * @author stefano.cattolica
+ */
+public class Frequenze {
+    private int n;
+
+    public Frequenze() {
+    }
+
+    public Frequenze(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+    
+    public double frequenzaRelativa(){
+        for(int i = 0;i<n;i++){
+            double r= 0;
+            r = Math.random()
+            
+        }
+    }
+    
+}
